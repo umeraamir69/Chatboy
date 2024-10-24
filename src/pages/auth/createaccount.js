94e11e55cefd877b4112fbdd896fe5d9dd3bf884
umer <umeraamir45@gmail.com>
2024-10-24 18:41:01 +0500

    chnage in create account

--- src/pages/auth/createaccount.js
+++ src/pages/auth/createaccount.js
@@ -14,13 +14,7 @@ import User from '../../../model/User';
 const Createaccount = ({ providers }) => {
 
     const [data, setdata] = useState({fname: "" ,lname : "" , email : "" })
-    const [recaptchaValue, setRecaptchaValue] = useState(null);
-    const [captchav2, setcaptchav2] = useState(false)
     const [providerval, setproviderval] = useState()
-    const handleRecaptcha = (value) => {
-        setRecaptchaValue(value);
-    };
-
     useEffect(() => {
         setproviderval(Object.values(providers))
     }, [providers])
@@ -90,8 +84,8 @@ const Createaccount = ({ providers }) => {
                 </div>
                 <div className="w-full md:w-1/2 py-10 px-5 md:px-10">
                     <div className="text-center mb-10">
-                        <h1 className="font-bold text-3xl text-gray-900">Join Our Network of Innovators</h1>
-                        <p className='mx-10 pt-5'>&quot;Unlock a world of possibilities by creating your account with devXpertize&quot;</p>
+                        <h1 className="font-bold text-3xl text-gray-900">Join Our Chat Boyt</h1>
+                        <p className='mx-10 pt-5'>&quot;Unlock a world of possibilities by creating your account with chatboy&quot;</p>
                     </div>
                     <form onSubmit={handleSignIn}>
                         <div className="flex -mx-3">
@@ -145,11 +139,7 @@ const Createaccount = ({ providers }) => {
                         </svg>
                     </button>
 
-                    <button aria-label="Log in with GitHub" className="p-3 rounded-sm"  key={providerval[0].name}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current"  onClick={() => signIn(providerval[0].id)}>
-                            <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
-                        </svg>
-                    </button>
+
 
                 </div>
                 : "No Provider is avalible"}
